9280cdcfe01642082ca02739a883d48e1b7af7b3
Gabriel Marini <105209654+DaleMarcio@users.noreply.github.com>
2024-04-30 11:10:17 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
-        for (let j = 0; j < botoes.length; j++) {
+
+for (let i = 0; i<botoes.length; i++) {
+    botoes[i].onclick = function(){
+        for(let j=0; j<botoes.length; j++){
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
@@ -11,9 +12,19 @@ for (let i = 0; i < botoes.length; i++) {
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-04-19T23:59:59");
+const tempoObjetivo1 = new Date("2024-10-15T23:59:59");
+const tempoObjetivo2 = new Date("2025-11-03T23:59:59");
+const tempoObjetivo3 = new Date("2025-08-05T23:59:59");
+const tempoObjetivo4 = new Date("2025-02-02T23:59:59");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [
+    tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
+]
+
+for (let i=0; i< contadores.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
+}
+contadores[0].textContent = calculaTempo(tempoObjetivo1)
 
 function calculaTempo(tempoObjetivo1){
     let tempoAtual = new Date();
@@ -26,9 +37,8 @@ function calculaTempo(tempoObjetivo1){
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-
-return dias + "dias"
-    + horas + "hora"
-    + minutos + "minutos"
-    + segundos + "segundos";
+    return dias + "dias"
+        + horas + "horas"
+        + minutos + "minutos"
+        + segundos + "segundos";
 }
